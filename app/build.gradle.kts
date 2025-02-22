9d646d2d397414b660fd561e2dde7f009a98ddea
jexner <jan.exner@gmx.net>
2025-02-23 00:06:28 +0100

    in which our hero updates and restricts permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -39,6 +39,11 @@ android {
     }
 
     buildToolsVersion = "35.0.0"
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
 
     applicationVariants.all {
         val variant = this
